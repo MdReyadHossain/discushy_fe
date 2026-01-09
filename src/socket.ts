401749b943da073c073e4692a342a24529591e2a
MdReyadHossain <reyadhosen@gmail.com>
2026-01-09 11:53:27 +0600

    environt variable has been implemented for socket connection

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,5 @@
 import { io } from "socket.io-client";
 
-// const URL = "http://localhost:3100"; // Local development server URL
-const URL = "https://ai-interview-3o-be.onrender.com"; // Production server URL
+const URL = import.meta.env.VITE_BACKEND_API_URL;
 
 export const socket = io(URL);
